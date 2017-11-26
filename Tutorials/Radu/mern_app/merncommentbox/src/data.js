cb6783bfa0e331f2b3de8c7cc561fe067a7f8aba
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2017-11-26 15:45:53 +0200

    Finish front end part

---
+++ Tutorials/Radu/mern_app/merncommentbox/src/data.js
@@ -0,0 +1,9 @@
+//data.js
+const data = [
+  { id: 1, author: 'Bryan', text: 'Wow this is neat!'},
+  { id: 2, author: 'You', text: 'youre __right!__'},
+  { id: 3, author: 'Neatu', text: 'Pointeri'},
+  { id: 4, author: 'Andra', text: 'RAX'}
+ ]
+
+ module.exports = data;
